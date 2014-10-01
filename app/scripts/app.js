fbf87b98978fcecac99c3a086fdcf0383f30966e
itkreativa <itkreativa@users.noreply.github.com>
2014-10-01 13:48:39 +0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name 20141001Html5JsSetupApp
+ * @description
+ * # 20141001Html5JsSetupApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('20141001Html5JsSetupApp', [
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 20141001Html5JsSetupApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the 20141001Html5JsSetupApp
+ */
+angular.module('20141001Html5JsSetupApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
